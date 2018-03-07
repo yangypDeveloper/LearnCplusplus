afcbb33bd7b94cdf6f7debcdadb4983b6fe1e374
Yoli <yangyp@fableedu.com>
2018-03-07 16:51:46 +0800

    hello

---
+++ hello.cpp
@@ -0,0 +1,13 @@
+/**
+ * platform:macOS 10.13.3 
+ * 2018年03月03日11:54:00
+ * Yoli
+ * 编译 g++ hello.cpp 
+ **/
+#include <iostream>
+using namespace std;
+int main(int argc, char *argv[])
+{
+    cout<<"Hello Wolrd!\n"<<endl;
+    return 0;
+}
